f089b59eac57c4547cfebe94313dceb66f8d022a
astelmashenko <andrey.stelmashenko@gmail.com>
2024-02-11 12:27:16 +0200

    made period volatile

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,19 +2,21 @@
 #![no_main]
 #![no_std]
 
-use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile::Volatile;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let half_period = 500_u16;
+    let mut half_period = 500_u16;
+    let v_half_period = Volatile::new(&mut half_period);
 
     loop {
         leds[0].on().ok();
-        delay.delay_ms(half_period);
+        delay.delay_ms(v_half_period.read());
 
         leds[0].off().ok();
-        delay.delay_ms(half_period);
+        delay.delay_ms(v_half_period.read());
     }
 }
